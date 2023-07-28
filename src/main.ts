32ecdc452408cdb91094dc3481c9fffae0bd40f5
KirillGenin <332211op@gmail.com>
2023-07-28 16:54:46 +0300

    feat: implemented getting the value of port from .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const PORT = configService.get('PORT') || 4000;
+
+  await app.listen(PORT, () => {
+    console.log(`Server started on ${PORT} port!`);
+  });
 }
+
 bootstrap();
