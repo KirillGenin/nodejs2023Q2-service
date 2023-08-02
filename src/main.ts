f45ebbc7cea67d3131a1e2a267dfd4fbbca9c8bb
KirillGenin <332211op@gmail.com>
2023-08-02 14:27:03 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,24 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { readFile } from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const file = await readFile(
+    join(dirname(__dirname), 'doc', 'api.yaml'),
+    'utf-8',
+  );
+
+  const doc = parse(file) as OpenAPIObject;
+
+  SwaggerModule.setup('doc', app, doc);
 
   const configService = app.get<ConfigService>(ConfigService);
 
