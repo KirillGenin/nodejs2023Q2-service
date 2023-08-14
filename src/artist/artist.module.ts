6e1e5a01cd46212f09c4b1513380ce4db682e27f
KirillGenin <332211op@gmail.com>
2023-08-15 01:18:04 +0300

    feat: transfer artist database to postgress

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -5,12 +5,12 @@ import { ArtistRepository } from './artist.repository';
 import { TrackModule } from '../track/track.module';
 import { AlbumModule } from 'src/album/album.module';
 import { FavsModule } from 'src/favs/favs.module';
-// import { ArtistEntity } from './entities/artist.entity';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArtistEntity } from './entities/artist.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    // TypeOrmModule.forFeature([ArtistEntity]),
+    TypeOrmModule.forFeature([ArtistEntity]),
     forwardRef(() => TrackModule),
     forwardRef(() => AlbumModule),
     forwardRef(() => FavsModule),
