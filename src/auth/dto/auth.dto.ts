ad2e6e491d0720d239b16269f31b8ac3faeb5994
KirillGenin <332211op@gmail.com>
2023-08-21 19:38:16 +0300

    chore: add AuthDto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,1 +1,11 @@
-export class AuthDto {}
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class AuthDto {
+  @IsNotEmpty()
+  @IsString()
+  login: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
