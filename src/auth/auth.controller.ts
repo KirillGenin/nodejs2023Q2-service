6ba6b8d2c68fcd46a51bf77399f2f50b7236a6f9
KirillGenin <332211op@gmail.com>
2023-08-21 20:41:31 +0300

    feat: implement signUp

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import {
-  Controller,
-  Post,
-} from '@nestjs/common';
+import { Controller, Post, HttpCode, HttpStatus, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  async signup(@Body() authDto: AuthDto) {
+    return await this.authService.signUp(authDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,24 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  forwardRef,
+  Inject,
+} from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor (private userService: UserService) {}
+  constructor(
+    @Inject(forwardRef(() => UserService))
+    private userService: UserService,
+  ) {}
+
+  async signUp(authDto: AuthDto) {
+    const { login, password } = authDto;
+    return await this.userService.create({
+      login,
+      password,
+    });
+  }
 }
